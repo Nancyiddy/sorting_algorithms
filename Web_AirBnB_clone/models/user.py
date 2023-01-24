da9ef01e536fee28c7dcaa1400fd39747595669d
Nancy Idiong <80394179+Nancyiddy@users.noreply.github.com>
2023-01-24 11:31:47 +0100

    Add files via upload

--- Web_AirBnB_clone/models/user.py
+++ Web_AirBnB_clone/models/user.py
@@ -1,1 +1,11 @@
-kjjc
+#!/usr/bin/python3
+"""contains User class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class based off BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
